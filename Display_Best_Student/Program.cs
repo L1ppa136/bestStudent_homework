29746fd4998a19cba1684293e7632058bbf853c2
Peter Mark Lippai <peterm.lippai@gmail.com>
2024-02-22 13:22:43 +0100

    Adding files

---
+++ Display_Best_Student/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Globalization;
+
+namespace Display_Best_Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string csvFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Data", "STUDENT_RESULTS.csv");
+            Student bestStudent = FindBestStudent(csvFilePath);
+            DisplayBestStudent(bestStudent);
+        }
+        
+        static Student FindBestStudent(string csvFilePath)
+        {
+            Student student = new Student("",float.MinValue,float.MinValue);
+
+            using (TextFieldParser parser = new TextFieldParser(csvFilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";");
+
+                // Skip header row
+                parser.ReadLine();
+
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    string name = fields[0];
+                    float avgTest = float.Parse(fields[1]);
+                    float avgYear = float.Parse(fields[2]);
+
+                    if (avgYear > student.AverageAnnual || (avgYear == student.AverageAnnual && avgTest > student.AverageTestResult))
+                    {
+                        student.Name = name;
+                        student.AverageAnnual = avgYear;
+                        student.AverageTestResult = avgTest;
+                    }
+                }
+            }
+
+            return student;
+        }
+
+        static void DisplayBestStudent(Student student)
+        {
+            Console.WriteLine($"The best student is {student.Name} with an average test result of {student.AverageTestResult} and an annual score of {student.AverageAnnual}.");
+        }
+    }
+}
---
+++ Display_Best_Student/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_Best_Student
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public float AverageTestResult { get; set; }
+        public float AverageAnnual {  get; set; }
+
+        public Student(string name, float averageTestResult, float averageAnnual) 
+        {
+            Name = name;
+            AverageTestResult = averageTestResult;
+            AverageAnnual = averageAnnual;
+        }
+    }
+}
